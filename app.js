61879e1fea2fb659013fc0c76740ee579cc58098
Nikita Remeslov <creatorbon@gmail.com>
2019-06-17 22:32:44 +0300

    add second dice to the game

--- app.js
+++ app.js
@@ -14,32 +14,39 @@ const RESET_VALUE = 1;
 let scores = [0, 0];
 let activePlayer = 0;
 let current = 0;
-const diceElement = document.querySelector('.dice');
+const firstDiceElement = document.querySelector('#dice-1');
+const secondDiceElement = document.querySelector('#dice-2');
 
 const initGame = () => {
   document.querySelector('#current-0').textContent = 0;
   document.querySelector('#current-1').textContent = 0;
   document.querySelector('#score-0').textContent = 0;
   document.querySelector('#score-1').textContent = 0;
-  diceElement.style.display = 'none';
+  firstDiceElement.style.display = 'none';
+  secondDiceElement.style.display = 'none';
 }
 
 initGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  let dice = Math.floor(Math.random() * 6) + 1;
+  let firstDice = Math.floor(Math.random() * 6) + 1;
+  let secondDice = Math.floor(Math.random() * 6) + 1;
 
-  diceElement.src = `./img/dice-${dice}.png`;
-  diceElement.style.display = 'block';
+  let diceSum = firstDice + secondDice;
 
-  if (dice !== RESET_VALUE) {
-    current += dice;
+  firstDiceElement.src = `./img/dice-${firstDice}.png`;
+  firstDiceElement.style.display = 'inline-block';
+
+  secondDiceElement.src = `./img/dice-${secondDice}.png`;
+  secondDiceElement.style.display = 'inline-block';
+
+  if (diceSum !== RESET_VALUE) {
+    current += diceSum;
     document.getElementById('current-'+activePlayer).textContent = current;
 
-    if (scores[activePlayer] + current >= 20) {
+    if (scores[activePlayer] + current >= 100) {
       alert(`Player ${activePlayer} won!!!`);
-    }
-    
+    } 
   } else {
     changePlayer();
   }
@@ -50,7 +57,7 @@ const changePlayer = () => {
   document.getElementById('current-'+activePlayer).textContent = 0;
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
   activePlayer = +!activePlayer;
-  diceElement.style.display = 'none';
+  firstDiceElement.style.display = 'none';
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
 }
 
