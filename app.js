41ca7ef1f04e7a850aff5d20d95767372e91ff94
Nikita Remeslov <creatorbon@gmail.com>
2019-06-18 14:57:12 +0300

    implementation of adding game limits

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 const RESET_VALUE = 2;
+const GAME_LIMIT = 100;
+const firstDiceElement = document.querySelector('#dice-1');
+const secondDiceElement = document.querySelector('#dice-2');
 
 let scores = [0, 0];
 let activePlayer = 0;
 let current = 0;
-const firstDiceElement = document.querySelector('#dice-1');
-const secondDiceElement = document.querySelector('#dice-2');
+let gameLimitValue = 0;
 
 const initGame = () => {
   document.querySelector('#current-0').textContent = 0;
   document.querySelector('#current-1').textContent = 0;
   document.querySelector('#score-0').textContent = 0;
   document.querySelector('#score-1').textContent = 0;
+  document.querySelector('.btn-roll').removeAttribute('disabled')
+  document.querySelector('.btn-hold').removeAttribute('disabled')
+  gameLimitValue = parseInt(document.querySelector('input').value);
   firstDiceElement.style.display = 'none';
   secondDiceElement.style.display = 'none';
-}
 
-initGame();
+  if (isNaN(gameLimitValue)) {
+    gameLimitValue = GAME_LIMIT;
+  }
+}
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   let firstDice = Math.floor(Math.random() * 6) + 1;
   let secondDice = Math.floor(Math.random() * 6) + 1;
-
   let diceSum = firstDice + secondDice;
 
   firstDiceElement.src = `./img/dice-${firstDice}.png`;
@@ -44,7 +39,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     current += diceSum;
     document.getElementById('current-'+activePlayer).textContent = current;
 
-    if (scores[activePlayer] + current >= 100) {
+    if (scores[activePlayer] + current >= gameLimitValue) {
       alert(`Player ${activePlayer} won!!!`);
     } 
   } else {
